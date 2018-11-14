3e5cabc50e11f43d487d066eea7d2f005003212d
Joel Ross <joelross@uw.edu>
2018-11-14 10:43:38 -0800

    Add post-lecture demo (fully complete)

--- interactive-react/src/App.js
+++ interactive-react/src/App.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props); //pass up to parent
+
+    //initialize state
+    this.state = {
+      tasks: [] //SAMPLE_TASKS //store the tasks in the STATE, initialize
+    };
+
+  }
+
+  componentDidMount() {
+    //fetch('https://api.github.com/repos/info340a-au18/lecture-demos/issues')
+    fetch('./tasks.json') //local file
+      .then((res) => res.json())
+      .then((data) => {
+        let issueTasks = data.map((issue) => {
+          return {
+            id: issue.id,
+            description: issue.description,
+            complete: false
+          }
+        })
+        this.setState({tasks:issueTasks})        
+      })
+  }
+
+  toggleComplete = (taskId) => {
+    this.setState((currentState) => { //update the state and RE-RENDER
+      let updatedTasks = currentState.tasks.map((task) => {
+        if(task.id === taskId)
+          task.complete = !task.complete;      
+        return task;
+      })
+      return {tasks: updatedTasks}
+    })
+  }
+
+  addTask = (newDescription) => {
+    this.setState((currentState) => {
+      let newTask = {
+        id: currentState.tasks.length+1,
+        description: newDescription,
+        complete: false
+      }
+      currentState.tasks.push(newTask); //add task, better to copy array
+      return {tasks: currentState.tasks}
+    })
+  }
+
   render() {
     //do data processing
-    let incomplete = this.props.tasks.filter((task) => !task.complete);
+    let incomplete = this.state.tasks.filter((task) => !task.complete);
     //console.log("Number of tasks:", incomplete.length);
 
     return (
       <div className="container">
         <p className="lead">Things I have to do ({incomplete.length})</p>
-        <TaskList tasks={this.props.tasks} />
-        <AddTaskForm />
+        <TaskList howToToggle={this.toggleComplete} tasks={this.state.tasks} />
+        <AddTaskForm howToAdd={this.addTask} />
       </div>
     );
   }
@@ -20,7 +69,11 @@ class TaskList extends Component {
   render() {
     //do data processing
     let taskComponents = this.props.tasks.map((eachTask) => {
-      let singleTask = <Task key={eachTask.id} task={eachTask} />
+      let singleTask = <Task 
+                          key={eachTask.id} 
+                          task={eachTask} 
+                          howToToggle={this.props.howToToggle}
+                          /> //pass callback down
       return singleTask;
     })
 
@@ -42,10 +95,14 @@ class Task extends Component {
     return className;    
   }
 
+  handleClick = () => {
+    this.props.howToToggle(this.props.task.id);
+  }
+
   render() {
     let thisTask = this.props.task; //can give local name for readability
     return (
-      <li className={this.getClassName()} >
+      <li className={this.getClassName()} onClick={this.handleClick} >
         {thisTask.description}
       </li>
     );
@@ -53,14 +110,36 @@ class Task extends Component {
 }
 
 class AddTaskForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {newTask: ''}; //what is typed in
+  }
+
+  handleChange = (event) => {    
+    //let whichElement = event.target;
+    //let whatValue = whichElement.value;
+    let value = event.target.value;
+    console.log("I changed to:", value);
+    this.setState({newTask: value})
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.props.howToAdd(this.state.newTask)
+  }
+
   render() {
     return (
       <form>
         <input 
           className="form-control mb-3"
           placeholder="What else do you have to do?"
+          value={this.state.newTask}
+          onChange={this.handleChange}
           />
-        <button className="btn btn-primary">Add task to list</button>
+        <button className="btn btn-primary" onClick={this.handleClick}>
+          Add task to list
+        </button>
       </form>
     );
   }
